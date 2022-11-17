446e1cda89ddb2f4bb1bef70b34a6f5be2b52993
yoonhero <yoonhero06@naver.com>
2022-11-17 10:05:18 +0900

    feat: Documentation and Modulize

--- README.md
+++ README.md
@@ -32,3 +32,22 @@
     - [ ] Login for wathing matching status
     - [ ] User matching Page
     - [ ] Chatting Page 
+
+
+## Theotreical Background
+
+### Cosine Similarity
+
+
+> In data analysis, cosine similarity is a measure of similarity between two sequences of numbers. [Learn More](https://en.wikipedia.org/wiki/Cosine_similarity)
+
+
+$similarity=cos(Θ)=\frac{A⋅B}{||A||\ ||B||}=\frac{\sum_{i=1}^{n}{A_{i}×B_{i}}}{\sqrt{\sum_{i=1}^{n}(A_{i})^2}×\sqrt{\sum_{i=1}^{n}(B_{i})^2}}$
+
+
+I thought that when I express the information of user in n-dimenstion, I can calculate the simliarity between the vector. I used cosine simliarity. I try to give a weight depending on the importance during matching. After some experiments, I had a several problems.
+
+- Weight didn't work well. As a result, it was matching between men and men.
+- The angle of two vector didn't represent that each users match well.
+
+The conclusion is that cosine similarity doesn't correspond with my purpose.
---
+++ ml/cosine_similarity.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
+
+
+class CosineSimilarity():
+    def __init__(self, dataframe, weight=100):
+        self.answers = [["남자", "여자"], ["17살", "18살"], ["INFP", "ENFP", 'ESFJ', "ISFJ", "ISFP", "ESFP", "INTP", "INFJ", "ENFJ", "ENTP", "ESTJ", "ISTJ", "INTJ", "ISTP", "ESTP", "ENTJ"],
+                        ["A", "B", "AB", "O"],
+                        ["한식", "양식", "중식", "일식"],
+                        ["빨강", "주황", "노랑", "초록", "파랑", "보라", "흰색", "검은색"]]
+
+        self.weight = weight
+
+        self.df = dataframe
+
+        self.schoolNumber_labels = self.df[:, 0]
+        self.schoolNumber_labels.rename(None, inplace=True)
+
+        self.index_dataframe = self.df.iloc[:, 1:].apply(
+            self.get_index, axis=1)
+
+        self.cosine_similarity_matrix = np.array([[self.cos_sim(np.array(self.index_dataframe[a]), np.array(
+            self.index_dataframe[b])) for a in range(len(self.index_dataframe))] for b in range(len(self.index_dataframe))])
+
+        for i in self.cosine_dataframe.index:
+            self.cosine_dataframe.loc[i][i] = 0
+
+    def get_index(self, df):
+        new_row = []
+        for idx, answer in enumerate(self.answers):
+            no_blank = df[idx].strip()
+            weighted_i = answer.index(no_blank) * self.weight / idx
+            new_row.append(weighted_i)
+
+        return new_row
+
+    def cos_sim(self, A, B):
+        return dot(A, B)/(norm(A)*norm(B))
+
+    def show(self):
+        self.cosine_dataframe.head(10)
+
+    def matching(self):
+        temp = self.cosine_dataframe.copy()
+
+        self.matching_status = []
+        for schoolNumber in self.schoolNumber_labels:
+            similairty = temp.loc[schoolNumber, :]
+
+            if similairty.max() == 0:
+                continue
+
+            max_similairty_index = similairty.idxmax()
+
+            temp.loc[schoolNumber, :] = 0
+            temp.loc[:, schoolNumber] = 0
+            temp.loc[max_similairty_index, :] = 0
+            temp.loc[:, max_similairty_index] = 0
+
+            self.matching_status.append([schoolNumber, max_similairty_index])
+
+        return self.matching_status
