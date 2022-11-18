50490d8c8a407e45beba94efc0959f3a49ea7c07
yoonhero <yoonhero06@naver.com>
2022-11-18 23:48:10 +0900

    feat: Matching System Module

--- ml/matching_system.py
+++ ml/matching_system.py
@@ -1,23 +1,17 @@
 import pandas as pd
 import numpy as np
-from numpy import dot
-from numpy.linalg import norm
+from cosine_similarity import cosine_similarity
+from pearson_corr import pearson_similarity
 
 
-def cosine_similarity(a, b):
-    return np.dot(a, b)/(norm(a) * norm(b))
-
-
-class CosineSimilarity():
-    def __init__(self, dataframe, weight=100):
+class RecommendSystem():
+    def __init__(self, csv_path):
         self.answers = [["남자", "여자"], ["17살", "18살"], ["INFP", "ENFP", 'ESFJ', "ISFJ", "ISFP", "ESFP", "INTP", "INFJ", "ENFJ", "ENTP", "ESTJ", "ISTJ", "INTJ", "ISTP", "ESTP", "ENTJ"],
                         ["A", "B", "AB", "O"],
                         ["한식", "양식", "중식", "일식"],
                         ["빨강", "주황", "노랑", "초록", "파랑", "보라", "흰색", "검은색"]]
 
-        self.weight = weight
-
-        self.df = dataframe
+        self.df = pd.read_csv(csv_path)
 
         self.schoolNumber_labels = self.df.iloc[:, 0]
         self.schoolNumber_labels.rename(None, inplace=True)
@@ -25,31 +19,37 @@ class CosineSimilarity():
         self.index_dataframe = self.df.iloc[:, 1:].apply(
             self.get_index, axis=1)
 
-        self.cosine_similarity_matrix = np.array([[self.cos_sim(np.array(self.index_dataframe[a]), np.array(
-            self.index_dataframe[b])) for a in range(len(self.index_dataframe))] for b in range(len(self.index_dataframe))])
-
-        self.cosine_dataframe = pd.DataFrame(self.cosine_similarity_matrix, columns=self.schoolNumber_labels, index=self.schoolNumber_labels)
-
-        for i in self.cosine_dataframe.index:
-            self.cosine_dataframe.loc[i][i] = 0
-
     def get_index(self, df):
         new_row = []
+
         for idx, answer in enumerate(self.answers):
             no_blank = df[idx].strip()
-            weighted_i = answer.index(no_blank) * self.weight / idx
-            new_row.append(weighted_i)
+
+            new_row.append(answer.index(no_blank))
 
         return new_row
 
-    def cos_sim(self, A, B):
-        return dot(A, B)/(norm(A)*norm(B))
+    def recommend(self, method):
+        if method == "cosine":
+            self.result_matrix = np.array([[cosine_similarity(np.array(self.index_dataframe[a]), np.array(
+                self.index_dataframe[b])) for a in range(len(self.index_dataframe))] for b in range(len(self.index_dataframe))])
 
-    def show(self):
-        self.cosine_dataframe.head(10)
+        elif method == "pearson":
+            self.result_matrix = np.array([[pearson_similarity(np.array(self.index_dataframe[a]), np.array(
+                self.index_dataframe[b])) for a in range(len(self.index_dataframe))] for b in range(len(self.index_dataframe))])
+
+        self.result_dataframe = pd.DataFrame(
+            self.result_matrix, columns=self.schoolNumber_labels, index=self.schoolNumber_labels)
+
+        self.clear_center()
+        self.result_dataframe.to_csv("result.csv")
+
+    def clear_center(self):
+        for i in self.result_dataframe.index:
+            self.result_dataframe.loc[i][i] = 0
 
     def matching(self):
-        temp = self.cosine_dataframe.copy()
+        temp = self.result_dataframe.copy()
 
         self.matching_status = []
         for schoolNumber in self.schoolNumber_labels:
@@ -68,3 +68,11 @@ class CosineSimilarity():
             self.matching_status.append([schoolNumber, max_similairty_index])
 
         return self.matching_status
+
+
+if __name__ == "__main__":
+    recommend = RecommendSystem("./sample_data.csv")
+
+    recommend.recommend("cosine")
+
+    print(recommend.matching())
